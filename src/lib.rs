39360cf079d59210cb769ca640b27180ebc9f103
Elijah Wilson <elijah.wilson@hookactions.com>
2020-05-07 15:42:03 -0400

    use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -24,17 +24,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        let case_sensitive = if args.len() >= 4 {
-            !args.contains(&String::from("--insensitive"))
-        } else {
-            env::var("CASE_INSENSITIVE").is_err()
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a file name"),
+        };
+        let case_sensitive = match args.next() {
+            Some(arg) => arg != String::from("--insensitive"),
+            None => env::var("CASE_INSENSITIVE").is_err(),
         };
 
         Ok(Config {
@@ -46,28 +49,19 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
